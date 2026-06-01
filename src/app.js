47fd360d830d5047cea9e70d468e0cfa1d1ec9a4
Soumyashree-Soumyajit-Baral <soumyashreebaral086@gmail.com>
2026-06-01 18:33:40 +0530

    Added frontend url to the cors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [
+      "https://afmairrack.netlify.app",
+      "http://localhost:5173"
+    ],
     credentials: true,
   })
 );
